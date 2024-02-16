21a9888d32d0ca0694f733e489a021b6b2a39bb1
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-02-16 17:34:41 -0300

    Adiciona arquivos CSS e define variáveis de estilo

--- views/site/__partials/head.php
+++ views/site/__partials/head.php
@@ -2,5 +2,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="css/output.css">
+    <link rel="stylesheet" href="css/variables.css">
+    <link rel="stylesheet" href="css/elements.css">
+    <link rel="stylesheet" href="css/classes.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
